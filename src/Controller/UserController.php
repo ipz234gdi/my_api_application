52a091417d66863a519ff6ddd3c7ca122532693e
Reiclid <reiclid@gmail.com>
2025-02-02 01:46:31 +0200

    update symfony 7.2 > 6.4 because many Errors

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -10,9 +9,10 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 
-#[Route('/api/v1')]
+#[Route('/api/v1', name: 'api_v1_')]
 class UserController extends AbstractController
 {
 
